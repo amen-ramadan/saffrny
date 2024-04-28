25029ea7d15d641e4ecb685548f77118f793e76a
unknown <ramadanamen407@gmail.com>
2024-04-28 22:59:33 +0300

    depug 4

--- src/js/index.js
+++ src/js/index.js
@@ -37,3 +37,79 @@ document.addEventListener('DOMContentLoaded', () => {
         var copyright = document.getElementById('copyright');
         copyright.innerHTML = "&copy; " + currentYear + "سفرني";
     });
+
+
+
+  // تعريف الدالة التي ستتم تنفيذها عند النقر على الزر
+//   function toggleContent() {
+//   var contents = document.getElementsByClassName("contentToToggle");
+//   for (var i = 0; i < contents.length; i++) {
+//     if (contents[i].style.display === "none" || contents[i].style.display === "") {
+//       contents[i].style.display = "block";
+//     } else {
+//       contents[i].style.display = "none";
+//     }
+//   }
+//   document.getElementById("toggleButton").classList.toggle("open");
+// }
+
+
+//   // اضافة حدث النقر إلى الزر
+//   document.getElementById("toggleButton").addEventListener("click", toggleContent);
+
+// <div class="icon-burger" id="toggleButton">
+//         <span></span><span></span><span></span>
+//       </div>
+
+
+// burger icon
+// .icon-burger {
+//   margin: 20px auto;
+//   width: 20px;
+//   height: 14px;
+//   cursor: pointer;
+//   position: relative;
+//   display: none;
+
+//   @include breakpoint(medium) {
+//     display: block;
+//   }
+//   span {
+//     background-color: black;
+//     height: 2px;
+//     position: absolute;
+//     width: 100%;
+//     left: 0;
+//     transition: all 0.3s ease;
+//   }
+
+//   span:first-child {
+//     top: 0;
+//   }
+
+//   span:nth-child(2) {
+//     top: 6px;
+//   }
+
+//   span:nth-child(3) {
+//     top: 12px;
+//   }
+
+//   &.open span:nth-child(2) {
+//     opacity: 0;
+//   }
+
+//   &.open span:first-child,
+//   &.open span:last-child {
+//     background-color: red;
+//     top: 6px;
+//   }
+
+//   &.open span:first-child {
+//     transform: rotate(45deg);
+//   }
+
+//   &.open span:last-child {
+//     transform: rotate(-45deg);
+//   }
+// }
