f8fd59ade0e0f8cdbccc0d35eabbddb0757c77f5
amen ramadan <amen@MacbookShops-MacBook-Pro.local>
2024-05-03 16:47:05 +0300

    images small than

--- webpack.config.js
+++ webpack.config.js
@@ -1,140 +1,108 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const TerserPlugin = require('terser-webpack-plugin');
+const { WebpackManifestPlugin } = require('webpack-manifest-plugin');
 const path = require("path");
 
 module.exports = {
-    entry: "./src/js/index.js",
-
-    output: {
-        // هذا الامر معناه ان المسار العام لانتاج ملفات المشروع هو الجذر
-        publicPath: '/',
-        path: path.resolve(__dirname, "build"),
-        filename: "js/bundle.js",
-        // هذان السطران لجعل الملفات اكثر ترتيبا في مجلد build
-        hotUpdateChunkFilename: 'hot/hot-update.js',
-        hotUpdateMainFilename: 'hot/hot-update.json',
-    },
-
-
-    //هذا الكود من موقع ستاك اوفر فلو من اجل جعل الموقع يعمل في حالة build  
-    //أي ليس في حالة التطوير وقد ظهر لي خطأ بعدم القدرة على تشغيل الملفات بسبب الحجم
-    performance: {
-        hints: false,
-        maxEntrypointSize: 512000,
-        maxAssetSize: 512000
-    },
-
-    module: {
-        rules: [
-            {
-                test: /\.(sass|css|scss)$/,
-                use: [
-                    // Creates `style` nodes from JS strings
-                    {
-                        loader: MiniCssExtractPlugin.loader,
-                        options: {
-                            publicPath: "../",
-                        },
-                    },
-                    // Translates CSS into CommonJS
-                    "css-loader",
-                    // Compiles Sass to CSS
-                    "sass-loader",
-                ],
-            },
-
-            {
-                test: /\.(png|jpe?g|gif|avif)$/i,
-
-                use: [
-                    {
-                        loader: "file-loader",
-
-                        options: {
-                            // هنا سيحفظ اسم الصورة واللاحقة الخاصة بها
-                            name: "[name].[ext]",
-                            // هنا اسم الملف الذي ستحفظ به الصور
-                            outputPath: "images",
-                        },
-                    },
-                ],
-            },
-
-            {
-                test: /\.svg$/,
-                use: 'raw-loader'
-              },
-
-            {
-                test: /\.(svg|eot|woff|woff2|ttf)$/,
-                use: [
-                    {
-                        loader: 'file-loader',
-                        options: {
-                            name: '[name].[ext]',
-                            outputPath: "fonts",
-                        }
-                    },
-                ],
-            },
-            {
-
-                test: /\.html$/i,
-
-                loader: 'html-loader',
-
-            },
-            {
-                test: require.resolve("jquery"),
-                loader: "expose-loader",
-                options: {
-                    exposes: ["$", "jQuery"],
-                },
-            },
+  entry: "./src/js/index.js",
+  output: {
+    publicPath: '/',
+    path: path.resolve(__dirname, "build"),
+    filename: "js/bundle.js",
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [new TerserPlugin()],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(sass|css|scss)$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          "sass-loader",
         ],
-    },
-    // هذا الجزء الخاص بتشغيل الموقع على السيرفر وايضا قمنا بتثبيت حزمة webpack dev server
-    devServer: {
-
-        static: {
-            directory: path.join(__dirname, " build"),
+      },
+      {
+        test: /\.(png|jpe?g|gif|avif)$/i,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "[name].[ext]",
+              outputPath: "images",
+            },
+            },
+            
+        ],
+      },
+      {
+        test: /\.svg$/,
+        use: 'raw-loader'
+      },
+      {
+        test: /\.(svg|eot|woff|woff2|ttf)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: "fonts",
+            }
+          },
+        ],
+      },
+      {
+        test: /\.html$/i,
+        loader: 'html-loader',
+      },
+      {
+        test: require.resolve("jquery"),
+        loader: "expose-loader",
+        options: {
+          exposes: ["$", "jQuery"],
         },
-
-        // من اجل اجبار السيرفر على اعادة التحميل عند كل تغيير
-        watchFiles: path.join(__dirname, 'src'),
-
-        compress: true, // هذا السطر لضغط الملفات وقمنا بتعليقه
-        port: 9000,
-        open: true,
-        devMiddleware: {
-            writeToDisk: true, // اكتب لنا الملفات اثناء تعديلها ..ليظهر هذا الامر الملف  build
-            stats: "errors-only", //  هذا السطر ليلغي الرسائل الزائدة في التيرمينال او موجه الاوامر وهي الرسائل الخاصة بعمليات  نجاح المهمات ويعرض لنا رسائل الاخطاء فقط واعني  كل هذا في حالة المشاهدة   
-        },
-    },
-
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: "./src/index.html",
-            filename: "index.html",
-        }),
-        new HtmlWebpackPlugin({
-            template: "./src/the-tourist-tour.html",
-            filename: "the-tourist-tour.html",
-        }),
-        new HtmlWebpackPlugin({
-            template: "./src/the-tourist-tour-2.html",
-            filename: "the-tourist-tour-2.html",
-        }),
-        new HtmlWebpackPlugin({
-            template: "./src/register.html",
-            filename: "register.html",
-        }),
-        new HtmlWebpackPlugin({
-            template: "./src/login.html",
-            filename: "login.html",
-        }),
-        new MiniCssExtractPlugin({
-            filename: "css/style.css", // اسم الملف النهائي الذي تريده والذي ينتج من ترجمة ملف ال scssفي src
-        }),
+      },
     ],
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "build"),
+    },
+    watchFiles: path.join(__dirname, 'src'),
+    port: 9000,
+    open: true,
+    devMiddleware: {
+      writeToDisk: true,
+      stats: "errors-only",
+    },
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+      filename: "index.html",
+    }),
+    new HtmlWebpackPlugin({
+      template: "./src/the-tourist-tour.html",
+      filename: "the-tourist-tour.html",
+    }),
+    new HtmlWebpackPlugin({
+      template: "./src/the-tourist-tour-2.html",
+      filename: "the-tourist-tour-2.html",
+    }),
+    new HtmlWebpackPlugin({
+      template: "./src/register.html",
+      filename: "register.html",
+    }),
+    new HtmlWebpackPlugin({
+      template: "./src/login.html",
+      filename: "login.html",
+    }),
+    new MiniCssExtractPlugin({
+      filename: "css/style.css",
+    }),
+    new WebpackManifestPlugin(),
+  ],
 };
